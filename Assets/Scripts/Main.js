167e4b0ba448b2f7ef91893cf2fa22a37d2290a2
Jason Mathias <jason.mathias@gmail.com>
2015-09-04 14:59:42 -0700

    Added project settings to staging, and added some instruction text in 'Main'.

--- Assets/Scripts/Main.js
+++ Assets/Scripts/Main.js
@@ -1,5 +1,20 @@
 ﻿#pragma strict
 
+/* Your task - write the following features:
+
+
+Press S to summon minion. 
+function - summonMinion().
+When minion summoned, he says "I do your bidding, my lord!"
+the number of minions you have (a variable you must declare) increases by 1). 
+
+Press A to attack with minions you have. 
+Game says "Ataaaaaaaaaaack!"
+run function attackDragon. 
+
+
+*/
+
 public var dragon: Dragon;
 public var minion: Minion;
 
