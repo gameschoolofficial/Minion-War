efb41b080126adec11d62718cec01f7035855796
Jason Mathias <jason.mathias@gmail.com>
2015-09-04 15:41:27 -0700

    minor edit

--- Assets/Scripts/Main.js
+++ Assets/Scripts/Main.js
@@ -3,14 +3,14 @@
 /* Your task - write the following features:
 
 
-Press S to summon minion. 
-function - summonMinion().
-When minion summoned, he says "I do your bidding, my lord!"
-the number of minions you have (a variable you must declare) increases by 1). 
+Press S to summon minion. When player presses 's' -> 
+-function - summonMinion()
+-the number of minions you have (a variable you must declare) increases by 1). 
+-Game says "summoning minion..You now have x", with 'x' being the number you have. 
 
-Press A to attack with minions you have. 
-Game says "Ataaaaaaaaaaack!"
-run function attackDragon. 
+Press A to attack with minions you have. When player presses 'a' ->
+-Game says "Ataaaaaaaaaaack!"
+-run function attackDragon. 
 
 
 */
